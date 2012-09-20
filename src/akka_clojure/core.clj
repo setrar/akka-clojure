8f7d27ffc6583289c1eecbae6f4365f1bf88be0a
Jason Gustafson <jason@Jasons-MacBook-Air.local>
2012-09-20 01:53:00 -0700

    Added basic supervision functionality

--- src/akka_clojure/core.clj
+++ src/akka_clojure/core.clj
@@ -2,7 +2,9 @@
        :doc "Simple clojure library for interacting with Akka actors"}
   akka-clojure.core
   (:import
-   [akka.actor ActorRef ActorSystem Props UntypedActor UntypedActorFactory OneForOneStrategy]
+   [akka.actor ActorRef ActorSystem Props
+    UntypedActor UntypedActorFactory
+    OneForOneStrategy SupervisorStrategy]
    [akka.japi Function]
    [akka.pattern Patterns]
    [akka.dispatch Await]
@@ -15,6 +17,13 @@
 (def ^:dynamic self nil)
 
 (defstruct duration :unit :value)
+
+(def escalate (SupervisorStrategy/escalate))
+(def stop (SupervisorStrategy/stop))
+(def resume (SupervisorStrategy/resume))
+(def restart (SupervisorStrategy/restart))
+
+
 
 (defn to-millis [duration]
   (.convert TimeUnit/MILLISECONDS (:value duration) (:unit duration)))
@@ -31,38 +40,58 @@
 (defn wait [future]
   (Await/result future (Duration/create 5 TimeUnit/SECONDS)))
 
+(defn !
+  "Send a message to an actor."
+  [actor msg]
+  (.tell actor msg))
+
 (defn reply
   "Reply to the sender of a message. Can ONLY be used from within an actor."
   [msg]
   (if (nil? self)
     (throw (RuntimeException. "Reply can only be used in the context of an actor"))
     (let [sender (.getSender self)]
-      (.tell sender msg))))
+      (! sender msg))))
 
-(defn one-for-one [max-retries fun]
-  (let [function (proxy [Function] []
+(defn one-for-one
+  ([fun] (one-for-one fun -1 (Duration/Inf)))
+  ([fun max-retries within-time-range]
+     (let [function (proxy [Function] []
 		   (apply [t] (fun t)))]
-    (proxy [OneForOneStrategy] [max-retries (Duration/Inf) function])))
+       (proxy
+	   [OneForOneStrategy]
+	   [max-retries within-time-range function]))))
       
-(defn !
-  "Send a message to an actor."
-  [actor msg]
-  (.tell actor msg))
-		     
-(defn- make-actor [context fun]
-  (.actorOf context
-	    (.withCreator
-	     (Props.)
-	     (proxy [UntypedActorFactory] []
-	       (create
-		[] (proxy [UntypedActor] []
-		     (onReceive [msg]
-				(binding [self this]
-				  (fun msg)))))))))
+
+(defn- make-actor
+  ([context fun]
+     (make-actor context nil fun))
+  ([context supervisor-strategy fun]
+     (.actorOf
+      context
+      (.withCreator
+       (Props.)
+       (proxy [UntypedActorFactory] []
+	 (create []
+		 (proxy [UntypedActor] []
+		   (supervisorStrategy
+		    []
+		    (if (nil? supervisor-strategy)
+		      (proxy-super supervisorStrategy)
+		      supervisor-strategy))
+		   (onReceive
+		    [msg]
+		    (binding [self this]
+		      (fun msg))))))))))
 
 (defn actor 
   "Create a new actor. If called in the context of another actor,
 this function will create a parent-child relationship."
-  [fun]
-  (make-actor (if (nil? self) *actor-system* self) fun))
+  ([fun]
+     (actor fun nil))
+  ([fun supervisor-strategy]
+     (make-actor
+      (if (nil? self) *actor-system* (.getContext self))
+      supervisor-strategy
+      fun)))
       
--- test/akka_clojure/test/core.clj
+++ test/akka_clojure/test/core.clj
@@ -12,3 +12,14 @@
 	val (wait (ask a "foo" (millis 500)))]
     (is (= "bar" val))))
 
+(deftest supervisor-works
+  (let [child #(throw (Exception. %1))
+	supervisor (actor #(let [c (actor child)]
+			     (! c %1)
+			     (reply "ok"))
+			  (one-for-one
+			   #(do (println (.getMessage %1)) stop)))
+	val (wait (ask supervisor "hi" (millis 500)))]
+    (is (= "ok" val))))
+
+
