63bfe9f71b762a26f14ad3e5c86a758e3f39e479
Jason Gustafson <jason.gustafson@gmail.com>
2012-10-30 21:00:28 -0700

    mark version for release

--- project.clj
+++ project.clj
@@ -1,5 +1,9 @@
-(defproject akka-clojure "1.0.0-SNAPSHOT"
+(defproject akka/akka-clojure "0.1.0"
   :description "An interface for using Akka through Clojure"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
 		 [com.typesafe.akka/akka-actor "2.0.3"]]
+  :url "https://github.com/jasongustafson/akka-clojure"
+  :license {:name "Eclipse Public License"
+	    :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories {"akka.repository" "http://repo.typesafe.com/typesafe/releases/"})
+    
