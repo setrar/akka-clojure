35856b162b52cf93bd1ac236b0e631e286bf9c12
Jack Lund <jackl@geekheads.net>
2014-10-11 17:28:30 -0500

    Make work with Akka 2.3.6

--- project.clj
+++ project.clj
@@ -2,11 +2,10 @@
   :description "An interface for using Akka through Clojure"
   :dependencies [[org.clojure/clojure "1.4.0"]
 		 [org.clojure/core.match "0.2.0-alpha11"]
-		 [com.typesafe.akka/akka-actor "2.0.3"]]
+		 [com.typesafe.akka/akka-actor_2.11 "2.3.6"]]
   :url "https://github.com/jasongustafson/akka-clojure"
   :license {:name "Eclipse Public License"
 	    :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories {"akka.repository" "http://repo.typesafe.com/typesafe/releases/"}
-  :plugins [[no-man-is-an-island/lein-eclipse "2.0.0"]]
-)
-    
+  :plugins [[no-man-is-an-island/lein-eclipse "2.0.0"]])
+
